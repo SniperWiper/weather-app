4be7fa5d26ba1e67028132470075487788c9832e
harshvardhan V <harshvazhuthi@gmail.com>
2025-05-15 16:31:44 +0530

    complete without forecast button feature

---
+++ index.js
@@ -0,0 +1,103 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import axios from 'axios'
+
+const port = 3000;
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+const directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
+                    "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function generateContent(content){
+    const date = new Date();
+    const newcontent ={};
+    newcontent.date = date.getDate();
+    newcontent.month = months[date.getMonth()];
+    newcontent.temp = Math.round(content.main.temp - 273.15);
+    newcontent.weather= content.weather[0].main;
+    newcontent.condition = content.weather[0].description;
+    newcontent.mintemp = (content.main.temp_min- 273.15).toFixed(2);
+    newcontent.maxtemp = (content.main.temp_max- 273.15).toFixed(2);
+    newcontent.feelslike = (content.main.feels_like- 273.15).toFixed(2);
+    newcontent.humidity = content.main.humidity;
+    newcontent.pressure = content.main.pressure;
+    newcontent.windspeed = (content.wind.speed*3.6).toFixed(2); 
+    const index = Math.floor((content.wind.deg + 11.25) / 22.5) % 16;
+    newcontent.wind_dir= directions[index];
+    newcontent.cloudcover = content.clouds.all;
+    if( content.rain && content.rain['1h']){
+        newcontent.rain = content.rain['1h']+" mm";
+    }else{
+        newcontent.rain = "None";
+    }
+
+    return newcontent;
+}
+
+function processForecast(forecast, address) {
+    const newforecast = [];
+    forecast.forEach(element => {
+        if (element.dt_txt.includes("12:00:00")) {
+            const content = generateContent(element);
+            const [year, month, day] = element.dt_txt.split(' ')[0].split('-'); // Extract date parts
+            content.date = parseInt(day); // Update the date field
+            content.month = months[parseInt(month) - 1]; // Update the month field using the months array
+            content.place = address;
+            newforecast.push(content);
+        }
+    });
+    return newforecast;
+}
+
+
+const yourAPI='a8d2ae81efa30f035ef17956e14e96dc';
+const googleAPI= 'AIzaSyCXaNcaN0DUotKgjX-5UsWEQJwRcEatdgI';
+
+app.get('/',async (req, res)=>{
+    try{
+        res.render("index.ejs", {
+            message: "Start Searching To get started!",
+            image: "search-city"
+    });
+    }catch(error){
+        console.log(error.message)
+        res.render("index.ejs", {
+            message: "Sever error!",
+            image: "not-found",
+        })
+    }
+})
+
+app.post('/search', async (req, res)=>{
+    try{
+        const response_google = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.place}&key=${googleAPI}`);
+        const location = response_google.data.results[0].geometry.location;
+        console.log(response_google.data.results[0]);
+        const latitude = location.lat;
+        const longitude = location.lng;
+        const address = response_google.data.results[0].formatted_address;
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${yourAPI}`);
+        const response_forecast = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${yourAPI}&days=7`)
+        const newcontent = generateContent(response.data);
+        const forecast = processForecast(response_forecast.data.list,address);
+        newcontent.place = address;
+        forecast[0]= newcontent;
+        //console.log(newcontent);
+        res.render("index.ejs",{
+            content: forecast,
+            index: 0
+    });
+    }catch(error){
+        console.log(error.message)
+        res.render("index.ejs",{
+            message: "No location found!",
+            image: "search-city"
+        });
+    }
+})
+
+app.listen(port, ()=>{
+    console.log("Listening at port: "+port);
+})
